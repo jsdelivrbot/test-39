b15303f5ae073fb5ae481d2ff29e7c72c2f5ef8a
Mykhailo Sukhorukov <m.sukhorukov@nodeart.io>
2017-11-03 01:16:32 +0200

    Initial commit

---
+++ source/js/views/App/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Routes from 'config/routes';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='Page'>
+          <Routes />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ source/js/views/Dashboard/index.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { testAction, testAsync } from 'actions/app';
+import CircleSvg from '../../../assets/svg/circle.svg';
+import SquareSvg from '../../../assets/svg/square.svg';
+import TriangleSvg from '../../../assets/svg/triangle.svg';
+import bookImg from '../../../assets/img/book2.jpg';
+
+@connect(state => ({
+  asyncData: state.app.get('asyncData'),
+  asyncError: state.app.get('asyncError'),
+  asyncLoading: state.app.get('asyncLoading'),
+  counter: state.app.get('counter'),
+}))
+export default class Dashboard extends Component {
+  static propTypes = {
+    asyncData: PropTypes.object,
+    asyncError: PropTypes.string,
+    asyncLoading: PropTypes.bool,
+    counter: PropTypes.number,
+    // from react-redux connect
+    dispatch: PropTypes.func,
+  }
+
+  constructor() {
+    super();
+
+    this.handleAsyncButtonClick = this.handleAsyncButtonClick.bind(this);
+    this.handleTestButtonClick = this.handleTestButtonClick.bind(this);
+  }
+
+  handleAsyncButtonClick() {
+    const { dispatch } = this.props;
+
+    dispatch(testAsync());
+  }
+
+  handleTestButtonClick() {
+    const { dispatch } = this.props;
+
+    dispatch(testAction());
+  }
+
+  render() {
+    const {
+      asyncData,
+      asyncError,
+      asyncLoading,
+      counter,
+    } = this.props;
+
+    return (
+      <div className='Dashboard'>
+        <h1>Marvin</h1>
+        <p>
+          Boilerplate for kicking off React/Redux applications.
+        </p>
+      </div>
+    );
+  }
+}
